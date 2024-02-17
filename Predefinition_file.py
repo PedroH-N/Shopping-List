20cd9c7033800a0a93af95ed37093a634b0bef5f
PedroH-N <pedronasci009@gmail.com>
2024-02-17 12:01:34 -0300

    Splited Classes
    I splited the classes Predefinition and Product to other files. I also took off from the Product's init method the part that handled with the predefinition list and sent it to new function in the main file.

---
+++ Predefinition_file.py
@@ -0,0 +1,18 @@
+
+class Predefinition:
+    # open_predef()
+
+    # show_predef()
+
+    # add_predef()
+
+    # remove_predef()
+
+    # clear_predef()
+
+    # start_checkmode()
+
+    # check_item()
+
+    # uncheck_item()
+    ...
--- lists_project.py
+++ lists_project.py
@@ -1,219 +1,120 @@
+from Product_file import Product
+from Predefinition_file import Predefinition
+import re
 
 # shopping list
 # store the predef list in a txt
-
-import re
 
 predefinition_list = [
     {"name": "Banana", "quantity": "12", "price": "15"}
 ]
 
-# class Predefinition:
 
-# open_predef()
+def write_over_choice_function(name, quantity, price=None):
+    while True:
+        write_over_choice = input(
+            "What do you want to update the product?\n[1] Just the name\n[2] Just the quantity\n[3] Just the price\n[4] Everything\n[5] Nothing\n"
+        )
 
-# show_predef()
+        digits = re.findall(
+            "[^12345]", write_over_choice, re.IGNORECASE)
 
-# add_predef()
+        if digits:
+            print(
+                "\nInvalid answer, it's just accepted number from 1 to 5.\n")
 
-# remove_predef()
+        if not digits and int(write_over_choice) == 5:
+            return {"name": name, "quantity": quantity, "price": price, "exit": 1}
 
-# clear_predef()
+        if not digits:
+            break
 
-# start_checkmode()
+    if not digits and int(write_over_choice) != 5:
+        write_over_choice = int(write_over_choice)
 
-# check_item()
+        if write_over_choice == 1:
+            name = Product.get_product()
 
-# uncheck_item()
+        elif write_over_choice == 2:
+            quantity = Product.get_quantity()
+
+        elif write_over_choice == 3:
+            price = Product.get_price_normal()
+
+        elif write_over_choice == 4:
+            name = Product.get_product()
+            quantity = Product.get_quantity()
+            price = Product.get_price_normal()
+
+    question_continue = input(
+        "Do you want to change something more about the item? (Y/N)")
+
+    digit = re.findall(
+        "(y|n)", question_continue, re.IGNORECASE)
+
+    digit_yes = re.search(
+        "y", question_continue, re.IGNORECASE)
+
+    digit_no = re.search(
+        "n", question_continue, re.IGNORECASE)
+
+    if len(digit) > 1:
+        print("Invalid answer, type just one character\n")
+
+    if not digit_yes and not digit_no:
+        print(
+            "Invalid answer, type Y or N (uppercase or lowercase)\n")
+
+    if digit_no and len(digit) == 1:
+        # break
+        return {"name": name, "quantity": quantity, "price": price, "exit": 1}
+
+    if digit_yes and len(digit) == 1:
+        return {"name": name, "quantity": quantity, "price": price, "exit": 0}
 
 
-class Product:
-
-    def __init__(self, name, quantity, price=None):
-        global predefinition_list
-        self.name = name
-        self.quantity = quantity
-        if price:
-            self.price = price
-
-        for product in predefinition_list:
-            if product["name"] == self.name:
-                flag_first_time = True
-                while True:
-                    if flag_first_time:
-                        print(
-                            "\nThe item is already in the predefinition as:\n"
-                            f"Name: {product['name']}\nQuantity: {product['quantity']}\n"
-                            f"Price: {product['price']}\n"
-                        )
-
-                    if not flag_first_time:
-                        print(
-                            "\nThe item is now set as:\n"
-                            f"Name: {product['name']}\nQuantity: {product['quantity']}\n"
-                            f"Price: {product['price']}\n"
-                        )
-
-                    # link instance variables among with the predefinitions
-                    self.name = product["name"]
-                    self.quantity = product["quantity"]
-                    self.price = product["price"]
-
-                    flag_first_time = False
-                    write_over_choice = input(
-                        "What do you want to update the product?\n[1] Just the name\n[2] Just the quantity\n[3] Just the price\n[4] Everything\n[5] Nothing\n"
+def product_in_list_check(name):
+    for product in predefinition_list:
+        if product["name"] == name:
+            flag_first_time = True
+            while True:
+                if flag_first_time:
+                    print(
+                        "\nThe item is already in the predefinition as:\n"
+                        f"Name: {product['name']}\nQuantity: {product['quantity']}\n"
+                        f"Price: {product['price']}\n"
                     )
 
-                    digits = re.findall(
-                        "[^12345]", write_over_choice, re.IGNORECASE)
+                if not flag_first_time:
+                    print(
+                        "\nThe item is now set as:\n"
+                        f"Name: {return_value_write_over['name']}\nQuantity: {return_value_write_over['quantity']}\n"
+                        f"Price: {return_value_write_over['price']}\n"
+                    )
+                    product['name'] = return_value_write_over['name']
+                    product["price"] = return_value_write_over["price"]
+                    product["quantity"] = return_value_write_over["quantity"]
 
-                    if digits:
-                        print(
-                            "Invalid answer, it's just accepted number from 1 to 5.")
+                flag_first_time = False
 
-                    if not digits and int(write_over_choice) == 5:
-                        break
+                return_value_write_over = write_over_choice_function(
+                    product["name"], product["quantity"], product["price"])
 
-                    if not digits and int(write_over_choice) != 5:
-                        write_over_choice = int(write_over_choice)
-                        if write_over_choice == 1:
-                
-                            product["name"] = Product.get_product()
-                        elif write_over_choice == 2:
-                            print(product["quantity"])
-                            
-                            product["quantity"] = Product.get_quantity()
-                            print(product["quantity"])
-                        elif write_over_choice == 3:
-                            print(product["price"])
-                            
-                            product["price"] = Product.get_price_normal()
-                            print(product["price"])
-                        elif write_over_choice == 4:
-                            product["name"] = Product.get_product()
-                            product["quantity"] = Product.get_quantity()
-                            product["price"] = Product.get_price_normal()
-
-                        question_continue = input(
-                            "Do you want to change something more about the item? (Y/N)")
-                        digit = re.findall(
-                            "(y|n)", question_continue, re.IGNORECASE)
-                        digit_yes = re.search(
-                            "y", question_continue, re.IGNORECASE)
-                        digit_no = re.search(
-                            "n", question_continue, re.IGNORECASE)
-
-                        # link instance variables among with the predefinitions
-                        self.name = product["name"]
-                        self.quantity = product["quantity"]
-                        self.price = product["price"]
-
-                        if len(digit) > 1:
-                            print("Invalid answer, type just one character\n")
-                        if not digit_yes and not digit_no:
-                            print(
-                                "Invalid answer, type Y or N (uppercase or lowercase)\n")
-
-                        if digit_no and len(digit) == 1:
-                            break
-
-    @classmethod
-    def get_product(cls):
-        while True:
-            product_name = input("Product name: ").strip().capitalize()
-            is_string = False
-
-            '''
-            try:
-                float(product_name)
-            except ValueError:
-                is_string = True
-
-            try:
-                int(product_name)
-            except ValueError:
-                is_string = True
-                
-            the int verification is working, but the float one isn't
-            so i change to the re.findall() because I didn't understand why it's not working
-            '''
-            digits = re.findall("[0-9]", product_name)  # re method
-
-            if not digits:
-                is_string = True  # help comment
-            else:
-                print("Invalid name, just accept alphabetical letters.\n")
-
-            if product_name and is_string:
-                break
-
-        return product_name
-
-    @classmethod
-    def get_quantity(cls):
-        while True:
-            product_quantity = input("Quantity: ").strip()
-            is_int = False
-
-            # g and k are accepted because equals gram or kilogram
-            digits = re.findall("[^0-9kg]", product_quantity, re.IGNORECASE)
-
-            if not digits:
-                is_int = True
-            else:
-                print(
-                    "Invalid quantity, just accepts numbers and the letters K and G.\n")
-
-            if product_quantity and is_int:
-                break
-
-        return product_quantity
-
-    @classmethod
-    def get_price_optional(cls):
-        while True:
-            can_exit = False
-
-            product_price_yn = input(
-                "Do you wan't to put the price? (Y/N) ").strip()
-
-            digit = re.findall("(y|n)", product_price_yn, re.IGNORECASE)
-            digit_yes = re.search("y", product_price_yn, re.IGNORECASE)
-            digit_no = re.search("n", product_price_yn, re.IGNORECASE)
-
-            if len(digit) > 1:
-                print("Invalid answer, type just one character\n")
-            if not digit_yes and not digit_no:
-                print("Invalid answer, type Y or N (uppercase or lowercase)\n")
-
-            if digit_no and len(digit) == 1:
-                break
-
-            if digit_yes and len(digit) == 1:
-
-                return Product.get_price_normal()
-
-    @classmethod
-    def get_price_normal(cls):
-        while True:
-            product_price = input("Price (write just the number): ")
-
-            is_int = False
-
-            digits = re.findall("[^0-9]", product_price, re.IGNORECASE)
-
-            if not digits:
-                is_int = True
-            else:
-                print("Invalid price")
-
-            if product_price and is_int:
-                return product_price
+                # the function just return a dict when you answer no to the question "do you want to change anything more?"
+                if isinstance(return_value_write_over, dict):
+                    if return_value_write_over["exit"] == 1:
+                        return [return_value_write_over["name"], return_value_write_over["quantity"], return_value_write_over["price"]]
 
 
 def main():
-    Product(Product.get_product(), Product.get_quantity(),
-            Product.get_price_optional())
+    product_isnt = Product(Product.get_product(), Product.get_quantity(),
+                           Product.get_price_optional())
+    new_values_return = product_in_list_check(product_isnt.name)
+
+    if isinstance(new_values_return, list):
+        product_isnt.name = new_values_return[0]
+        product_isnt.quantity = new_values_return[1]
+        product_isnt.price = new_values_return[2]
 
 
 if __name__ == "__main__":
